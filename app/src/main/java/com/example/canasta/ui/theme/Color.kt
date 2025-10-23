d6d3c11125f2540f307746e7157ccbae2a3e6edd
mgarciapuente <mgarciapuente@itba.edu.ar>
2025-10-23 14:35:52 -0300

    Set colors

--- app/src/main/java/com/example/canasta/ui/theme/Color.kt
+++ app/src/main/java/com/example/canasta/ui/theme/Color.kt
@@ -2,10 +2,10 @@ package com.example.canasta.ui.theme
 
 import androidx.compose.ui.graphics.Color
 
-val Purple80 = Color(0xFFD0BCFF)
-val PurpleGrey80 = Color(0xFFCCC2DC)
-val Pink80 = Color(0xFFEFB8C8)
-
-val Purple40 = Color(0xFF6650a4)
-val PurpleGrey40 = Color(0xFF625b71)
-val Pink40 = Color(0xFF7D5260)
+val Primary = Color(0xFF465D46)
+val Secondary = Color(0xFFF5844E)
+val Titles = Color(0xFF333333)
+val Background = Color(0xFFEBEEEB)
+val Warnings = Color(0xFFFFA726)
+val Errors = Color(0xFFD32F2F)
+val Success = Color(0xFF2E7D32)
